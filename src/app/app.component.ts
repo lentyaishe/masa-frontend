18c1cf06c40d290063d315696f274dad31c8cb0b
Ilya Pogrebensky <ilya@billboy.co.il>
2022-12-04 15:01:40 +0200

    Component basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from "@angular/core";
 
+interface IPerson {
+    name: string;
+    id: string;
+    address: string;
+    email: string;
+}
+
 @Component({
     selector: "app-root",
     templateUrl: "./app.component.html",
@@ -7,4 +14,26 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
     title = "masa-frontend";
+
+    public myProperty: string = "Hooray!!";
+    public htmlProperty: string = "<i>La-la!!</i>"
+
+    public person: IPerson = {
+        name: "John Doe",
+        id: "876598769",
+        address: "Somewhere in US",
+        email: "john@doe.com"
+    };
+
+    public cardMessage: string = "";
+
+    public onClickMeClick(): void {
+        this.myProperty = "New data!!";
+    }
+
+    public onCardModeChanged(isEdit: boolean): void {
+        this.cardMessage = isEdit ?
+            "Please, fill the data" :
+            "Data saved";
+    }
 }
