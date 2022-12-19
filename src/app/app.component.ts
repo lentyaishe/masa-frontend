2c76dd709175968379214a0cf4b16ef8e81a5763
Ilya Pogrebensky <ilya@billboy.co.il>
2022-12-19 11:35:10 +0200

    Document injection Rtl vs ltr with classes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { LayoutDirection } from "./enums";
+import { DOCUMENT } from "@angular/common";
+import { Component, Inject } from "@angular/core";
 import { States } from "./constants";
 import { LayoutService } from "./services/layout.service";
 
@@ -11,9 +13,23 @@ export class AppComponent {
     public States = States;
 
     public title: string = "masa-frontend";
-    public isRtl: boolean = false;
 
     constructor(
+        @Inject(DOCUMENT) private document: Document,
         public layoutService: LayoutService
-    ) {}
+    ) {
+        this.alignUi();
+    }
+
+    public onChangeDirectionClick(): void {
+        this.layoutService.layoutDirection = this.layoutService.layoutDirection === LayoutDirection.Ltr ? LayoutDirection.Rtl : LayoutDirection.Ltr;
+        this.alignUi();
+    }
+
+  private alignUi() {
+    const bodyElement: HTMLElement | null = this.document.getElementById("mf-body");
+    if (bodyElement) {
+      bodyElement.style.direction = this.layoutService.layoutDirection === LayoutDirection.Rtl ? "rtl" : "ltr";
+    }
+  }
 }
